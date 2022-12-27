b3a6211452aea854ac5e9c2da8f916677f1e05c1
kunal nimsatkar <kunal.nimsatkar@gmail.com>
2022-12-27 13:21:16 +0530

    bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import "./main.css";
-import template from "../src/template.html";
-document.write(template);
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 32,
+  loop: true,
+  coverflowEffect: {
+    rotate: 2,
+  },
+});
